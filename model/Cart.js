6e6c421395ea72534143a897f67d2e497d5e4715
viu <vishnu.kraman88@gmail.com>
2020-04-28 23:50:57 +0530

    fixing a delete productbug

--- model/Cart.js
+++ model/Cart.js
@@ -48,6 +48,9 @@ module.exports = class Cart {
       }
       const updatedCart = { ...JSON.parse(fileContent) };
       const product = updatedCart.products.find((product) => product.id === id);
+      if (!product) {
+        return;
+      }
       const productQty = product.qty;
       updatedCart.products = updatedCart.products.filter(
         (product) => product.id !== id
