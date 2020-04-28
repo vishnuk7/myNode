6bd78e7be02db3ee7cfd41d68e111f82675dbd50
viu <vishnu.kraman88@gmail.com>
2020-04-28 23:20:16 +0530

    deleting cart items

--- controller/shop.js
+++ controller/shop.js
@@ -36,6 +36,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
+  Cart.getCart((product) => {});
   res.render("shop/cart", {
     pageTitle: "Cart",
     path: "/cart",
